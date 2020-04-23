fc4232af08f547c1d7259d9de12530f4d68eef0b
VladYoSlav <46863908+VladYoSlav@users.noreply.github.com>
2020-04-23 16:44:15 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ import App from "./App";
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
 
+bridge.sendPromise('VKWebAppGetCommunityAuthToken', {
+    "app_id": 7427444, // id вашего свежесозданного mini App
+    "group_id": 189437939,  // id группы, где вы админ, куда виджет
+    "scope": "app_widget"
+  })
+  .then(data => {
+    // Обработка события в случае успеха
+    console.log(data);
+  })
+  .catch(error => {
+    //Обработка событияв случае ошибки
+  });
+
 ReactDOM.render(<App />, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
   import("./eruda").then(({ default: eruda }) => {}); //runtime download
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -24,7 +24,7 @@ const Home = ({ id, go, fetchedUser }) => (
 		<Group title="Navigation Example">
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Ненавижу блять JS
+					Show me the Persik, please
 				</Button>
 			</Div>
 		</Group>
