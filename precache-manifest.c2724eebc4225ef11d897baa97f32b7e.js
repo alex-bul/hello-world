7cabdb598a6416110ba60a6eee9ef8238c89a8bd
VladYoSlav <vaifaer@gmail.com>
2020-04-23 15:59:42 +0300

    Updates

---
+++ precache-manifest.c2724eebc4225ef11d897baa97f32b7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bec410a519951e430703442b23997bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4992e019bc38ed9ffefc",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "50d6953ae2fd4c3635e4",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4992e019bc38ed9ffefc",
+    "url": "./static/js/2.86f9db43.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.86f9db43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d6953ae2fd4c3635e4",
+    "url": "./static/js/main.3d01fec2.chunk.js"
+  },
+  {
+    "revision": "88f7e0bc6b03921ebc9a",
+    "url": "./static/js/runtime-main.77727976.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b96dfd22a41d1dbd322a584f08fa6b2c.js"
+  "./precache-manifest.c2724eebc4225ef11d897baa97f32b7e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
